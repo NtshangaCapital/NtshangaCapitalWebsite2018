e9914c7b8f74fa54741fd7e3058870491c623a02
SiphosethuLokwe <35679212+SiphosethuLokwe@users.noreply.github.com>
2018-03-01 15:52:20 +0200

    Add files via upload

--- EditProfile.php
+++ EditProfile.php
@@ -63,67 +63,97 @@ foreach($results as $result)
 
 
 //Update Customer Profile Picture
-        if(isset($_POST["btnimage"]))
-        {
-    //         if (!isset($_FILES['fileToUpload']['tmp_name']))
-    //         {
-    //  echo '<div class="alert">
-    //          <span class="closebtn" onclick="this.parentElement.style.display=none;">&times;</span> 
-    //       <strong>Danger!</strong> Something went wrong while updating please try again after 5 minutes.
-    //    </div>';      
-       
-    //    }
-
-        $Email = $_email;
-        $ProfilePircture = file_get_contents($_FILES['fileToUpload']['tmp_name']);
-        if(empty($ProfilePicture))
-        {
-           throw new Exception('picture needs to be selected');
-        }
-        $UpdatePicture = $custusername_datamaper->UpdateUserProfilePicture($Conn,$Comm,$Email,$ProfilePircture);
-      
-           if($UpdatePicture)
-           {
-             header("Refresh:0; url=EditProfile.php");
-              
-            }
-         else{
-            echo '<div class="alert">
-            <span class="closebtn" onclick="this.parentElement.style.display=none;">&times;</span> 
-         <strong>Danger!</strong> Something went wrong while updating please try again after 5 minutes.
-      </div>';         
-         }
-        } 
-        else
+            if(isset($_POST["btnimage"]) && isset($_FILES['fileToUpload'] ))
             {
-               
-                echo"you need to select file";
-                    
+
+                function get_file_extension($file_name)
+            {
+                return pathinfo($file_name , PATHINFO_EXTENTION);
             }
 
-       
+            function get_file_extension1($file_name)
+            {
+                return (explode('.', $file_name));
+            }
+
+            function getfilesize($file_name)
+            {
+                return (filesize($file_name));
+            }
+
+           
+            $path = $_FILES['fileToUpload']['name'];
+            $path2 =$_FILES['fileToUpload']['tmp_name'];
+            $ext = end(get_file_extension1($path));
+         
+            $filesize = getfilesize($path2);
+            $ProfilePircture = file_get_contents($_FILES['fileToUpload']['tmp_name']);
+
+                if($filesize > 2097152)
+                {
+                    echo '<div class="alert">
+                    <span class="closebtn" onclick="this.parentElement.style.display=none;">&times;</span> 
+                <strong>Danger!</strong> please select a photo with ext PNG/JPG/GIF with size smaller than 2MB .
+            </div>';   
+                    
+                }
+                else
+                {
+                if($ext == 'PNG' || $ext == 'Png' || $ext == 'jpeg' || $ext == 'JPEG' || $ext == 'GIF' || $ext == 'gif'  )
+                 {
+                     echo '<div class="alert">
+                     <span class="closebtn" onclick="this.parentElement.style.display=none;">&times;</span> 
+                 <strong>Danger!</strong> must be animage.
+             </div>';         
+
+                }
+                 else{
+                    $Email = $_email;
+                    $UpdatePicture = $custusername_datamaper->UpdateUserProfilePicture($Conn,$Comm,$Email,$ProfilePircture);
             
-                       
-        
-   
-class Validate{
-    public function __construct(){
-    }
-    public function GetFirstName(){
-        return filter_var($_POST["fname"], FILTER_SANITIZE_STRING);
-    }
-    public function GetLastName(){
-        return filter_var($_POST["lname"], FILTER_SANITIZE_STRING);
-    }
-    public function GetEmail(){
-        return filter_var($_POST["Email"], FILTER_SANITIZE_EMAIL);
-    }
-    public function GetContactNumber(){
-        return filter_var($_POST["contactno"],FILTER_SANITIZE_STRING);
-    }
-    public function GetPictutre(){
-        return filter_var($_FILES["fileToUpload"],FILTER_SANITIZE_BLOB);
-    }
+                    if($UpdatePicture)
+                    {
+                        header("Refresh:0; url=EditProfile.php");
+                        
+                        }
+                    else{
+                        echo '<div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display=none;">&times;</span> 
+                    <strong>Danger!</strong> Something went wrong while updating please try again after 5 minutes.
+                </div>';         
+                    }
+
+                }
+                   
+    
+                 }
+                }
+
+              
+                   
+
+            //  } 
+    
+            
+
+        class Validate{
+            public function __construct(){
+            }
+            public function GetFirstName(){
+                return filter_var($_POST["fname"], FILTER_SANITIZE_STRING);
+            }
+            public function GetLastName(){
+                return filter_var($_POST["lname"], FILTER_SANITIZE_STRING);
+            }
+            public function GetEmail(){
+                return filter_var($_POST["Email"], FILTER_SANITIZE_EMAIL);
+            }
+            public function GetContactNumber(){
+                return filter_var($_POST["contactno"],FILTER_SANITIZE_STRING);
+            }
+            public function GetPictutre(){
+                return filter_var($_FILES["fileToUpload"],FILTER_SANITIZE_BLOB);
+            }
 }
 
 ?>
@@ -192,7 +222,7 @@ class Validate{
 <div class="container">
       <div class="row">
       <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
-        <A href="edit.html" >Logout</A>
+        <A href="index.html" onclick="return confirm('Are you sure to logout?');" id ="logout" >Logout</A>
        <br>
 <p class=" text-info"> <?php echo date('d M Y ') ?> </p>
       </div>
@@ -292,7 +322,27 @@ for (i = 0; i < close.length; i++) {
     }
 }
 </script>
-<!-- <script>
+
+
+<script>
+function deleletconfig(){
+
+var del=confirm("Are you sure you want to delete this record?");
+if (del==true){
+   alert ("record deleted")
+}
+return del;
+}
+</script>
+
+//add onclick event
+onclick="return deleletconfig()"
+
+
+
+
+
+ <!-- <script>
 function myFunction() {
     var txt;
     var r = confirm("Are You sure You want to Submit");
@@ -305,11 +355,10 @@ function myFunction() {
         txt = "You pressed Cancel!";
     }
     document.getElementById("demo").innerHTML = txt;
-     window.location.href = 'profile.php';
 
-}
+} -->
 
 
-</script> -->
+<!-- </script> --> -->
 </body>
 </html>
